d3e16a448e8725b5deac7590b9ba9260a57c359c
marcoshenke <marcos.henke3@gmail.com>
2023-04-18 16:54:56 -0300

    changes in post_serializer

--- mini-blog/app/serializers/post_serializer.rb
+++ mini-blog/app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
+  attributes :id, :title, :description, :category_id, :user_id
 
   has_many :comments, serializer: CommentSerializer, if: -> { instance_options[:list_comments] }
 end
