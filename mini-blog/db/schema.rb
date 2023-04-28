9a4c6526bf4000a1cf3134c640de323b8d24e896
Marcos Henke <marcos.henke@dponet.com.br>
2023-04-28 12:05:23 -0300

    alteration in schema

--- mini-blog/db/schema.rb
+++ mini-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_201819) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_201633) do
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -66,7 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_201819) do
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["kind_user_id"], name: "index_users_on_kind_user_id"
