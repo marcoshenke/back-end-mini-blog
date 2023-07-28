67937cd39fd1c501980720e6e4b5ebdec09ffe74
Marcos Henke <marcos.henke@dponet.com.br>
2023-07-28 07:11:50 -0300

    faltou testar comentarios do search posts

--- mini-blog/spec/resources/posts/list_spec.rb
+++ mini-blog/spec/resources/posts/list_spec.rb
@@ -1,2 +1,64 @@
-class Posts::List
+require 'rails_helper'
+
+RSpec.describe Posts::List do
+  attr_accessor :posts
+
+  before(:all) do
+    @posts = FactoryBot.create_list(:post, 3)
+  end
+  describe '#initialize' do
+    before do
+      @instance = described_class.new(posts)
+    end
+
+    it 'returns instancied variables' do
+      expect(@instance.params).to eq(posts)
+    end
+  end
+
+  describe '#execute' do
+    context 'when searching by id' do
+      before do
+        params = { id: posts.first.id }
+        @response = described_class.new(params).execute
+      end
+
+      it 'should calls the __search method on Post with ID as a params' do
+        expect(@response).to include(posts.first)
+      end
+    end
+
+    context 'when searching title' do
+      before do
+        params = { title: posts.first.title }
+        @response = described_class.new(params).execute
+      end
+
+      it 'should calls the __search method on Post with title as a params' do
+        expect(@response).to include(posts.first)
+      end
+    end
+
+    context 'when searching description' do
+      before do
+        params = { description: posts.first.description }
+        @response = described_class.new(params).execute
+      end
+
+      it 'should calls the __search method on Post with description as a params' do
+        expect(@response).to include(posts.first)
+      end
+    end
+
+    context 'when searching comments' do
+      before do
+        params = { description: posts.comments_comment }
+        @response = described_class.new(params).execute
+      end
+
+      it 'should calls the __search method on Post with comments as a params' do
+        expect(@response).to include(posts.first)
+      end
+    end
+  end
 end
